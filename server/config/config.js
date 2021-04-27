4a8f8846f1a530ca01e8995afeea896aaeb5499e
Devante W <williams.devante05@gmail.com>
2021-04-26 22:37:20 -0500

    create ddb routes

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    region: 'us-east-2',
+    aws_dynamodb_table: 'Thoughts',
+    accessKeyId: 'xxxx',
+    secretAccessKey: 'xxxx'
+  };
+  
+  
---
+++ server/db/db_createTable.js
@@ -0,0 +1,76 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'us-east-2'});
+
+// Create the DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+var params = {
+  AttributeDefinitions: [
+    {
+      AttributeName: '_id',
+      AttributeType: 'S'
+    },
+    {
+      AttributeName: 'plantName',
+      AttributeType: 'S'
+    },
+    {
+      AttributeName: 'location',
+      AttributeType: 'S'
+    },
+    {
+      AttributeName: 'cost',
+      AttributeType: 'N'
+    },
+    {
+      AttributeName: 'description',
+      AttributeType: 'S'
+    },
+    {
+      AttributeName: 'createdAt',
+      AttributeType: 'S'
+    }
+  ],
+  KeySchema: [
+    {
+      AttributeName: '_id',
+      KeyType: 'HASH'
+    },
+    {
+        AttributeName: 'plantName',
+        AttributeType: 'HASH'
+      },
+      {
+        AttributeName: 'location',
+        AttributeType: 'HASH'
+      },
+      {
+        AttributeName: 'cost',
+        AttributeType: 'RANGE'
+      },
+      {
+        AttributeName: 'description',
+        AttributeType: 'RANGE'
+      },
+    
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 1,
+    WriteCapacityUnits: 1
+  },
+  TableName: 'PlantPosts',
+  StreamSpecification: {
+    StreamEnabled: false
+  }
+};
+
+// Call DynamoDB to create the table
+ddb.createTable(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Table Created", data);
+  }
+});
