602eaca063aa2a3f32d65f955209c505cec5c817
Devante W <williams.devante05@gmail.com>
2021-05-23 22:29:14 -0500

    add login capability

--- client/src/components/firebase.js
+++ client/src/components/firebase.js
@@ -2,14 +2,13 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderid: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+    apiKey: "AIzaSyDON71aFjRtNjJTs7-8-okTuKlUz5c9R-4",
+    authDomain: "plant-app-7ea76.firebaseapp.com",
+    projectId: "plant-app-7ea76",
+    storageBucket: "plant-app-7ea76.appspot.com",
+    messagingSenderId: "861267277415",
+    appId: "1:861267277415:web:4c6837ccd62f0d0a74c6c2",
+    measurementId: "G-7G8007NN47"
 })
 
 
